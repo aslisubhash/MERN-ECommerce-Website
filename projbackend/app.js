2655225b77fe1e7b9348d42ebfbdf925e03792e4
aslisubhash <myearning4me@gmail.com>
2020-04-10 10:58:11 +0530

    Second Commit

---
+++ projbackend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+
+//DB Connections
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("db connected.."))
+  .catch((err) => console.log(err));
+
+//POrt
+const port = process.env.PORT || 5500;
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
+
+//myroutes
+app.use("/api", authRoutes);
+
+//start server
+
+app.listen(port, () => {
+  console.log(`app is running at http://localhost:${port} ...`);
+});
